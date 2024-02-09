b4bef892970b120e3bc6a771a35e53e8dc47a7e0
Sebastian <sebastian.ternes@code.berlin>
2024-02-09 13:51:28 +0100

    Initial Commit

---
+++ Weather Ui Test/Weather_Ui_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_Ui_TestApp.swift
+//  Weather Ui Test
+//
+//  Created by Sebastian Ternes on 09.02.24.
+//
+
+import SwiftUI
+
+@main
+struct Weather_Ui_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
